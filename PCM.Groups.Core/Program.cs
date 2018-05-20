38c0e4a942e27d646152e2ef72eb8cb744c6ab70
mpedroc90 <mpedroc90@gmail.com>
2018-05-20 15:14:30 -0400

    Add project files.

---
+++ PCM.Groups.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PCM.Groups.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
